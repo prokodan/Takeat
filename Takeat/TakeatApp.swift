c0b610830f39c20e522b594d638534f65322ce8b
Danil Prokopenko <69258218+prokodan@users.noreply.github.com>
2023-05-22 17:12:51 +0600

    Initial Commit

---
+++ Takeat/TakeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeatApp.swift
+//  Takeat
+//
+//  Created by Данил Прокопенко on 22.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TakeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
